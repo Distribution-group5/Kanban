819f6655b6b09c0dc6f14e03f1e9f0cf1f2f4188
Peterzxcvbnm <peter.braendgaard@gmail.com>
2019-10-28 20:17:29 +0100

    Endpoint skeleton and startup

---
+++ KanbanBackend/Board/BoardController.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var boardRouter = express.Router();
+
+boardRouter.post('/DeleteBoard', function (req, res) {
+
+});
+
+boardRouter.post('/InviteToBoard', function (req, res) {
+
+});
+
+
+
+
+module.exports = boardRouter;
--- KanbanBackend/WebserverStart.js
+++ KanbanBackend/WebserverStart.js
@@ -1,1 +1,14 @@
+const webSocket = require('./Board/BoardWebSocket.js');
+var express = require('Express');
+var app = express();
+var boardController = require('./Board/BoardController.js');
+var userController = require('./User/UserController.js');
+
 Console.log("It is starting O.o");
+
+webSocket.startWebsocket();
+
+app.use('/Board', boardController);
+app.use('/User', userController);
+
+app.listen(80);
