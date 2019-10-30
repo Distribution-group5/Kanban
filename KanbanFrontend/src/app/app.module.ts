03b218c6076faf1fb26df9c70bf232dcc3f9ce7d
Robinlp <Robinlp@users.noreply.github.com>
2019-10-30 20:23:41 +0100

    board and card

--- KanbanFrontend/src/app/app.module.ts
+++ KanbanFrontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
 import { LoginComponent } from './login/login.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    LoginComponent
+    LoginComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
--- KanbanFrontend/src/app/board/board.component.ts
+++ KanbanFrontend/src/app/board/board.component.ts
@@ -7,56 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  kanbanBoard;
+  kanbanBoard = new KanbanBoard(1, "Example", [[new KanbanCard(1, "E", "content1", [], Date.now(), Date.now())], [new KanbanCard(2, "EE", "content2", [], Date.now(), Date.now())]]);
+
 
   constructor() { }
 
   ngOnInit() {
-    class KanbanBoard {
-      id: number;
-      Title: string;
-      Columns: [[]];
-
-      constructor(id, Title, Columns) {
-        this.id = id;
-        this.Title = Title;
-        this.Columns = Columns;
-      }
-
-      get NumberOfColumns() {
-        return this.Columns.length;
-      }
-
-      get NumberOfRows() {
-        let MaxRows = 0;
-        this.Columns.forEach(x => { if (x.length > MaxRows) MaxRows = x.length });
-        return MaxRows;
-      }
-
-      readJson(json) {
-        Object.assign(this, json);
-      }
-    }
-
-    class KanbanCard {
-      id: number;
-      Title: string;
-      Content: string;
-      Assignees: string[];
-      DateCreated: Date;
-      DateClosed: Date;
-
-      constructor(id, Title, Content, Assignees, DateCreated, DateClosed) {
-        this.id = id;
-        this.Title = Title;
-        this.Content = Content;
-        this.Assignees = Assignees;
-        this.DateCreated = DateCreated;
-        this.DateClosed = DateClosed;
-      }
-    }
-
-    this.kanbanBoard = new KanbanBoard(1, "Example", [new KanbanCard(1, "E", "", [], Date.now(), Date.now())]);
   }
 
+}
+
+class KanbanBoard {
+  id: number;
+  Title: string;
+  Columns: Array<Array<KanbanCard>>;
+
+  constructor(id, Title, Columns) {
+    this.id = id;
+    this.Title = Title;
+    this.Columns = Columns;
+  }
+
+  get NumberOfColumns() {
+    return this.Columns.length;
+  }
+
+  get NumberOfRows() {
+    let MaxRows = 0;
+    this.Columns.forEach(x => { if (x.length > MaxRows) MaxRows = x.length });
+    return MaxRows;
+  }
+
+  readJson(json) {
+    Object.assign(this, json);
+  }
+}
+
+class KanbanCard {
+  id: number;
+  Title: string;
+  Content: string;
+  Assignees: string[];
+  DateCreated: Date;
+  DateClosed: Date;
+
+  constructor(id, Title, Content, Assignees, DateCreated, DateClosed) {
+    this.id = id;
+    this.Title = Title;
+    this.Content = Content;
+    this.Assignees = Assignees;
+    this.DateCreated = DateCreated;
+    this.DateClosed = DateClosed;
+  }
+
+  toString(){
+    return "ID: "+ this.id + 
+    " Title: " + this.Title + 
+    " Content: " + this.Content + 
+    " Assignees: " + this.Assignees + 
+    " Date created: " + this.DateCreated + 
+    " Date closed: " + this.DateClosed; 
+  }
 }
