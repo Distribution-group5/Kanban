acdb0372fbc5eff80e0f9b1c3c245e5dd65a668b
Peterzxcvbnm <peter.braendgaard@gmail.com>
2019-10-30 18:50:18 +0100

    Delete board and invite user endpoints

--- KanbanBackend/DatabaseConnection.js
+++ KanbanBackend/DatabaseConnection.js
@@ -1,27 +1,34 @@
-const fs;
-const mysql;
+let fs;
+let mysql;
 let con;
 
-let startDBConnection = function () {
+let executeQuery = function (query) {
     fs = require('fs');
     mysql = require('mysql');
-    let dbInfo = fs.readFileSync('/DBInfo.txt', 'utf8');
+    let dbResult;
+    let dbInfo = fs.readFileSync('./DBInfo.txt', 'utf8');
     let dbInfoArray = dbInfo.split(",");
 
 
     con = mysql.createConnection({
         host: dbInfoArray[0],
         user: dbInfoArray[1],
-        password: dbInfoArray[2]
+        password: dbInfoArray[2],
+        database: 'KanbanDatabase'
     });
 
     con.connect(function (err) {
         if (err) throw err;
         console.log("Connected!");
+        con.query(query, function (err, result) {
+            if (err) throw err;
+            console.log(`Query complete`);
+        });
     });
+
+
 }
 
 module.exports = {
-    startDBConnection = startDBConnection,
-    connection = con
+    executeQuery: executeQuery,
 }
--- KanbanBackend/WebserverStart.js
+++ KanbanBackend/WebserverStart.js
@@ -1,15 +1,14 @@
 const webSocket = require('./Board/BoardWebSocket.js');
 const express = require('Express');
 const app = express();
-const DBConnection = require('./DatabaseConnection.js');
 const boardController = require('./Board/BoardController.js');
 const userController = require('./User/UserController.js');
 
 console.log("It is starting O.o");
 
-DBConnection.startDBConnection();
 webSocket.startWebsocket();
 
+app.use(express.json());
 app.use('/Board', boardController);
 app.use('/User', userController);
 
