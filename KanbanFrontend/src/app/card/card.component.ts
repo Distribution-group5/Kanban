6f9c1a4ceae9307e22a5a5315e42a6466b4b175e
Peterzxcvbnm <peter.braendgaard@gmail.com>
2019-11-03 16:49:53 +0100

    Editable card

--- KanbanFrontend/src/app/card/card.component.ts
+++ KanbanFrontend/src/app/card/card.component.ts
@@ -1,22 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { KanbanCard} from '../board/board.component';
+import { Component, OnInit, Input, NgModule, Output, EventEmitter } from '@angular/core';
+import { KanbanCard } from '../board/board.component';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css']
 })
+
 export class CardComponent implements OnInit {
-  
-  @Input() card: KanbanCard;
-  
+
+    @Input() card: KanbanCard;
+    @Output() cardUpdate = new EventEmitter<KanbanCard>();
+
+    raiseCardUpdate() {
+        console.log("Changed occured");
+        this.cardUpdate.emit(this.card);
+    }
+
+    autogrow(element) {
+        element.target.style.height = "0px";
+        element.target.style.height = (element.target.scrollHeight + 25) + "px";
+    }
 
 
-  constructor() {
-    
-   }
+    constructor() {
 
-  ngOnInit() {
-  }
+    }
+
+    ngOnInit() {
+    }
 
 }
+
