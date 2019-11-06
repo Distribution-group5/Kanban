46eaf6ce6209fadb2097d61599a942cb2bbddd67
Peterzxcvbnm <peter.braendgaard@gmail.com>
2019-11-06 17:25:56 +0100

    Create board

--- KanbanBackend/Board/BoardController.js
+++ KanbanBackend/Board/BoardController.js
@@ -48,12 +48,42 @@ boardRouter.get('/GetBoards', function (req, res, next) {
             if (err) next(err)
             else {
                 console.log(`get boards for: ${username} complete`);
-                
+
                 res.status(200).send(result);
             }
         });
     });
 
+});
+
+ttp://localhost:8080/Board/CreateBoard?username=user1&title=Test
+boardRouter.post('/CreateBoard', function (req, res, next) {
+    let username = req.query.username;
+    let title = req.query.title;
+    let boardID = Math.floor(Math.random() * 2000000000);
+    console.log("Trying to create board for:", username);
+    let con = mysql.createConnection({
+        host: dbInfoArray[0],
+        user: dbInfoArray[1],
+        password: dbInfoArray[2],
+        database: 'KanbanDatabase',
+        multipleStatements: true
+    });
+
+    con.connect(function (err) {
+        if (err) next(err);
+        let sql = `INSERT INTO Board VALUES (${boardID}, '${title}', ''); INSERT INTO UserBoardRelation VALUES ('${username}', ${boardID})`;
+        console.log("Connected!");
+        
+        con.query(sql, function (err, result) {
+            if (err) next(err)
+            else {
+                console.log(`Board (${title}) created for: ${username} complete`);
+                console.log(`Board (${title}) relation with: ${username} created`);
+                res.status(200).send();
+            }
+        });
+    });
 });
 
 boardRouter.post('/InviteToBoard', function (req, res, next) {
